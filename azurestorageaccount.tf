c0c0ae7103e1a8730f5574231b6bac1e50e08569
dasguptadebasishPierre <123695881+dasguptadebasishPierre@users.noreply.github.com>
2024-11-11 23:37:35 -0600

    Update azurestorageaccount.tf

--- azurestorageaccount.tf
+++ azurestorageaccount.tf
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 # Create public IP
-resource "azurerm_public_ip" "public" {
+resource "azurerm_public_ip" "publicip" {
   name                = "${var.prefix}TFPublicIP"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
